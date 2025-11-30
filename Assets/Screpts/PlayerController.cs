41d00f8e9be092c2de978ecd4e2641be595f2ffb
kiridumeuta <kiridume1125@gmail.com>
2025-11-30 22:29:59 +0900

    a

---
+++ Assets/Screpts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void PlayerMove()
+    {
+
+    }
+}
